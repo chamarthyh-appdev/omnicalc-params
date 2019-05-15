86fbf072b774085f60a76c859b3c60a06f66b015
Harish Chamarthy <harish.chamarthy@kellogg.northwestern.edu>
2019-05-15 06:05:11 +0000

    Both square links done

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -4,4 +4,14 @@ class CalcController < ApplicationController
         @a_number_sq = @a_number ** 2
         render("flex_templates/square.html.erb")
     end
+    
+    def square_form
+        render("form_templates/square.html.erb")
+    end 
+    
+    def process_square_form
+        @a_number = params.fetch("user_number").to_f
+        @a_number_sq = @a_number ** 2
+        render("results_templates/square.html.erb")
+    end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
     match("/flexible/square/:zebra", { :controller => "calc", :action => "flex_square", :via => "get"})
+    
+    match("/square/new", { :controller => "calc", :action => "square_form", :via => "get" })
+    
+    match("/square/results/", { :controller => "calc", :action => "process_square_form", :via => "get"})
 end
