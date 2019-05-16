c72b7bb4855a1c4bc24cd3b4b0db082e9c8aab91
Harish Chamarthy <harish.chamarthy@kellogg.northwestern.edu>
2019-05-16 01:38:52 +0000

    at 93 percent

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -14,4 +14,58 @@ class CalcController < ApplicationController
         @a_number_sq = @a_number ** 2
         render("results_templates/square.html.erb")
     end
+    
+    def flex_sqrt
+        @sqrt_input = params.fetch("sqrt").to_f
+        @sqrt_result = @sqrt_input ** (0.5)
+        render("flex_templates/square_root.html.erb")
+    end
+    
+    def sqrt_form
+        render("form_templates/square_root.html.erb")
+    end 
+    
+    def process_sqrt_form
+        @sqrt_input = params.fetch("user_sqrt_input").to_f
+        @sqrt_result = @sqrt_input ** (0.5)
+        render("results_templates/square_root.html.erb")
+    end
+    
+    def flex_pmt
+        @apr_input = params.fetch("apr").to_f/10000/12
+        @term_input = params.fetch("term").to_f*12
+        @principal_input = params.fetch("principal").to_f
+        @monthly_pmt = (@apr_input*@principal_input)/(1-((1+@apr_input) ** (-@term_input)))
+        render("flex_templates/payment.html.erb")
+    end
+    
+    def pmt_form
+        render("form_templates/payment.html.erb")
+    end
+    
+    def pmt_results
+        @apr_input = params.fetch("apr_input").to_f/10000/12
+        @term_input = params.fetch("years_input").to_f*12
+        @principal_input = params.fetch("principal_input").to_f
+        @monthly_pmt = (@apr_input*@principal_input)/(1-((1+@apr_input) ** (-@term_input)))
+        render("results_templates/payment.html.erb")
+    end
+    
+    def random
+        @min_input = params.fetch("min").to_i
+        @max_input = params.fetch("max").to_i
+        @rand_output = rand(@min_input..@max_input)
+        render("flex_templates/random.html.erb")
+    end 
+    
+    def random_form
+        render("form_templates/random.html.erb")
+    end
+    
+    def random_results
+        @min_input = params.fetch("min_input").to_f
+        @max_input = params.fetch("max_input").to_f
+        @rand_output = rand(@min_input..@max_input)
+        render("results_templates/random.html.erb")
+    end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,35 @@
 Rails.application.routes.draw do
+   
+   #Square Routes
+   
     match("/flexible/square/:zebra", { :controller => "calc", :action => "flex_square", :via => "get"})
     
     match("/square/new", { :controller => "calc", :action => "square_form", :via => "get" })
     
     match("/square/results/", { :controller => "calc", :action => "process_square_form", :via => "get"})
+    
+    #Square Root Routs
+    
+    match("/flexible/square_root/:sqrt", { :controller => "calc", :action => "flex_sqrt", :via => "get"})
+    
+    match("/square_root/new", { :controller => "calc", :action => "sqrt_form", :via => "get" })
+    
+    match("/square_root/results/", { :controller => "calc", :action => "process_sqrt_form", :via => "get"})
+    
+    #Payment Routes
+    
+    match("/flexible/payment/:apr/:term/:principal", { :controller => "calc", :action => "flex_pmt", :via => "get"})
+    
+    match("/payment/new", { :controller => "calc", :action => "pmt_form", :via => "get" })
+    
+    match("/payment/results/", { :controller => "calc", :action => "pmt_results", :via => "get"})
+    
+    #Random Routes
+    
+    match("/flexible/random/:min/:max", { :controller => "calc", :action => "random", :via => "get"})
+    
+    match("/random/new", { :controller => "calc", :action => "random_form", :via => "get"})
+    
+    match("/random/results", { :controller => "calc", :action => "random_results", :via => "get"})
+
 end
